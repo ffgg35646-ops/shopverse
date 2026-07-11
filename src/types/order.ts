31cd1d1fe8a882f0c95a26a3025c9dea216fad87
mohamed ali <ffgg35646@gmail.com>
2026-07-11 04:20:07 -0700

    order.ts

---
+++ src/types/order.ts
@@ -0,0 +1,56 @@
+import type { User } from "./user";
+
+export interface OrderItem {
+  productId: number;
+  name: string;
+  thumbnail: string;
+
+  price: number;
+  quantity: number;
+
+  color?: string;
+  size?: string;
+}
+
+export interface ShippingAddress {
+  fullName: string;
+  phone: string;
+
+  country: string;
+  city: string;
+
+  addressLine1: string;
+  addressLine2?: string;
+
+  postalCode: string;
+}
+
+export interface Order {
+  id: number;
+  orderNumber: string;
+
+  user: User;
+  items: OrderItem[];
+
+  shippingAddress: ShippingAddress;
+
+  subtotal: number;
+  shipping: number;
+  discount: number;
+  tax: number;
+  total: number;
+
+  paymentMethod: "Credit Card" | "PayPal" | "Cash on Delivery";
+
+  paymentStatus: "Pending" | "Paid" | "Failed";
+
+  orderStatus:
+    | "Pending"
+    | "Processing"
+    | "Shipped"
+    | "Delivered"
+    | "Cancelled";
+
+  createdAt: string;
+  updatedAt: string;
+}
