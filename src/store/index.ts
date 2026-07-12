7aa2b701230754263de7040fa371337b66e1b050
mohamed ali <ffgg35646@gmail.com>
2026-07-11 18:17:29 -0700

    index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector
+} from "react-redux";
 
 import cartReducer from "./cartSlice";
 import productReducer from "./productSlice";
@@ -27,5 +32,11 @@ export type RootState = ReturnType<
   typeof store.getState
 >;
 
-
 export type AppDispatch = typeof store.dispatch;
+
+
+export const useAppDispatch = () =>
+  useDispatch<AppDispatch>();
+
+export const useAppSelector: TypedUseSelectorHook<RootState> =
+  useSelector;
