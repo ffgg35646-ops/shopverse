f2cf700be06249a316fdb4b80102f5ac70fbb8e9
mohamed ali <ffgg35646@gmail.com>
2026-07-11 18:32:07 -0700

    index.ts

---
+++ src/data/index.ts
@@ -0,0 +1,19 @@
+export { navigation } from "./navigation";
+
+export { products } from "./products";
+
+export { reviews } from "./reviews";
+
+export { brands } from "./brands";
+
+export { categories } from "./categories";
+
+export { coupons } from "./coupons";
+
+export { faqs } from "./faqs";
+
+export { footer } from "./footer";
+
+export { sliders } from "./sliders";
+
+export { testimonials } from "./testimonials";
