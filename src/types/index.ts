6a88f6e3d12fb9162e66bd704cf4e71a247131b9
mohamed ali <ffgg35646@gmail.com>
2026-07-11 17:59:19 -0700

    index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,5 @@ export type { User } from "./user";
 export type { Order } from "./order";
 export type { Cart, CartItem } from "./cart";
 export type { Review } from "./review";
+export type { Brand } from "./brand";
+export type { Category } from "./category";
